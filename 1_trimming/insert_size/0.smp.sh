fd1f9113896c7014ca3685d5e8b59e30d41f6fc8
Devan Desai <93961382+ddesai-02@users.noreply.github.com>
2024-06-07 14:42:42 -0400

    Update 0.smp.sh

--- 1_trimming/insert_size/0.smp.sh
+++ 1_trimming/insert_size/0.smp.sh
@@ -6,12 +6,12 @@
 source ../variable.py
 SP=$sp
 RAW_DIR=raw_seq_dir_w.txt
-PATH=$path
+Path=$path
 ##
 ## 0.smp sampling: ###################################################################################################################################################
 ## Import your directories from windows in 'raw_seq_dir_w.txt'
 ## Set up compatibility
-tr -d '\15\32' < $PATH/$SP/$RAW_DIR > $PATH/$SP/raw_seq_dir.txt
+tr -d '\15\32' < $Path/$SP/$RAW_DIR > $Path/$SP/raw_seq_dir.txt
 ## Create directories:
 mkdir -p 0.smp/smp_seq/
 ## Construct the sampling.sh file:
@@ -19,9 +19,9 @@ echo "#!/bin/bash" > 0.smp/sampling.sh
 echo "#SBATCH --partition normal" >> 0.smp/sampling.sh
 echo "#SBATCH --mem-per-cpu 1G" >> 0.smp/sampling.sh
 ## Reads 1:
-cut -f 2 $PATH/$SP/raw_seq_dir.txt | while read p; do echo "perl sampleReads.pl $p | gzip -c > 0.smp/smp_seq/$(basename $p '.fq.gz').smp.fq.gz"; done >> 0.smp/sampling.sh
+cut -f 2 $Path/$SP/raw_seq_dir.txt | while read p; do echo "perl sampleReads.pl $p | gzip -c > 0.smp/smp_seq/$(basename $p '.fq.gz').smp.fq.gz"; done >> 0.smp/sampling.sh
 ## Reads 2:
-cut -f 3 $PATH/$SP/raw_seq_dir.txt | while read p; do echo "perl sampleReads.pl $p | gzip -c > 0.smp/smp_seq/$(basename $p '.fq.gz').smp.fq.gz"; done >> 0.smp/sampling.sh
+cut -f 3 $Path/$SP/raw_seq_dir.txt | while read p; do echo "perl sampleReads.pl $p | gzip -c > 0.smp/smp_seq/$(basename $p '.fq.gz').smp.fq.gz"; done >> 0.smp/sampling.sh
 ##
 ## Allow the execution of the file:
 chmod u+x 0.smp/sampling.sh
