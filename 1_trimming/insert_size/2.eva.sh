7c4da050342332019b84617d25f1ac5e94d43a4e
Devan Desai <93961382+ddesai-02@users.noreply.github.com>
2024-06-07 14:45:42 -0400

    Update 2.eva.sh

--- 1_trimming/insert_size/2.eva.sh
+++ 1_trimming/insert_size/2.eva.sh
@@ -7,7 +7,7 @@
 ##
 mkdir 2.eva
 echo '#!/bin/bash' > 2.eva/eva.sh
-echo '#SBATCH --partition normal' >> 2.eva/eva.sh
+echo '#SBATCH --account=rrg-shaferab' >> 2.eva/eva.sh
 echo '#SBATCH --mem 16G' >> 2.eva/eva.sh
 ls 1.map/map_seq | sed 's/_read.*//' | sort | uniq | while read p; do echo "evalInsertSize.pl 1.map/map_seq/"$p"_read_1.smp.bam 1.map/map_seq/"$p"_read_2.smp.bam > 2.eva/"$p".eis"; done >> 2.eva/eva.sh
 ##
