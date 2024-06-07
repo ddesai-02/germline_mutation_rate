5b3091ed660e72259334993e08d142f39a742216
Devan Desai <93961382+ddesai-02@users.noreply.github.com>
2024-06-07 14:39:25 -0400

    Update 1.summary_filtering.sh

--- 1_trimming/filtering/1.summary_filtering.sh
+++ 1_trimming/filtering/1.summary_filtering.sh
@@ -5,12 +5,12 @@
 source ../variable.py
 ##
 SP=$sp
-PATH=$path
+Path=$path
 ##
 ## Remove the output of cleanning:
 rm submit_clean*.out
 ## Again build a bash with all the bash for that
-ls -d $PATH/$SP/trimmed_seq/*/ | while read p; do echo "bash summary_SOAPnuke.sh $PATH/$SP/trimmed_seq/$(basename "$p" '/') $PATH/$SP/trimmed_seq"; done > run_summary_SOAPnuke.sh
+ls -d $Path/$SP/trimmed_seq/*/ | while read p; do echo "bash summary_SOAPnuke.sh $Path/$SP/trimmed_seq/$(basename "$p" '/') $Path/$SP/trimmed_seq"; done > run_summary_SOAPnuke.sh
 ##
 ##
 chmod u+rwx run_summary_SOAPnuke.sh
@@ -19,4 +19,4 @@ bash run_summary_SOAPnuke.sh
 ## Now you should have summary_SOAPnuke.txt in the output directory : $PATH/$SP/trimmed_seq/
 ##
 ## Write the directories of the trimmed files
-less $PATH/$SP/raw_seq_dir.txt | while read a b c; do echo -e "$a\t$(echo $b | sed -r "s:.fq.gz:.clean.fq.gz:g; s:fastq_files:trimmed_seq/$(basename "$b" '_read_1.fq.gz'):g;")\t$(echo $c | sed -r "s:.fq.gz:.clean.fq.gz:g; s:fastq_files:trimmed_seq/$(basename "$b" '_read_1.fq.gz'):g;")";done > $PATH/$SP/clean_seq_dir.txt
+less $Path/$SP/raw_seq_dir.txt | while read a b c; do echo -e "$a\t$(echo $b | sed -r "s:.fq.gz:.clean.fq.gz:g; s:fastq_files:trimmed_seq/$(basename "$b" '_read_1.fq.gz'):g;")\t$(echo $c | sed -r "s:.fq.gz:.clean.fq.gz:g; s:fastq_files:trimmed_seq/$(basename "$b" '_read_1.fq.gz'):g;")";done > $Path/$SP/clean_seq_dir.txt
