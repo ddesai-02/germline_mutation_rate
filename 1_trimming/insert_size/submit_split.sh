058362d232cf91e2debe0d9e4b4653be68050ead
Devan Desai <93961382+ddesai-02@users.noreply.github.com>
2024-06-07 14:47:18 -0400

    Update submit_split.sh

--- 1_trimming/insert_size/submit_split.sh
+++ 1_trimming/insert_size/submit_split.sh
@@ -6,9 +6,9 @@ echo "#!/bin/bash" > 1.map/mapping_splitted.sh
 while read p; do
         COUNT=$(( $COUNT + 1 ))
         echo "#!/bin/bash" > 1.map/submit_split/submit_map_$COUNT
-        echo "#SBATCH --partition normal" >> 1.map/submit_split/submit_map_$COUNT
+        echo "#SBATCH --account=rrg-shaferab" >> 1.map/submit_split/submit_map_$COUNT
         echo "#SBATCH --mem-per-cpu 5G" >> 1.map/submit_split/submit_map_$COUNT
-        echo "#SBATCH -c 4" >> 1.map/submit_split/submit_map_$COUNT
+        echo "#SBATCH -cpus-per-task=4" >> 1.map/submit_split/submit_map_$COUNT
         echo $p >> 1.map/submit_split/submit_map_$COUNT;
         ##
         echo "sbatch -o 1.map/submit_split/submit_map_$COUNT.out -e 1.map/submit_split/submit_map_$COUNT.out 1.map/submit_split/submit_map_$COUNT" >> 1.map/mapping_splitted.sh
