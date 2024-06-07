bcee95a73ed9ad7adae74e5388650c0233e64fc5
Devan Desai <93961382+DevBboy@users.noreply.github.com>
2024-06-07 13:44:57 -0400

    Update 1_write_pedigree.sh

--- 0_settings/1_write_pedigree.sh
+++ 0_settings/1_write_pedigree.sh
@@ -1,7 +1,7 @@
 # Write $PATH/$SPECIES/pedigree.ped
 # Format ped
 # Group Offspring Father Mother Sex Phenotype
-G1	AD139_M	AD127_M	AD081_F	M	NA
+G1  AD139_M	AD127_M	AD081_F	M	NA
 G1  AD076_M	AD127_M	AD081_F	M	NA
 G2  AD104_F	AD026_M	AD059_F	F	NA
 G3  AD108_F	AD014_M	AD110_F	F	NA
