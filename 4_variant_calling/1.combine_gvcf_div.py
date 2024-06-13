e14514f49d907d5c6105a94812833bd96bce0757
= <=>
2024-06-12 22:14:19 -0400

    Formatting

--- 4_variant_calling/1.combine_gvcf_div.py
+++ 4_variant_calling/1.combine_gvcf_div.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This script catenate the GVCF files per scaffold/chromosome for all individuals.
+This script catenate the GVCF files per scaffosome for all individuals.
 """
 #################
 # What you need #
@@ -40,28 +40,28 @@ for line in f:
 genome_assembly = pd.read_csv('{}scaffolds.txt'.format(scaff_dir),sep=' ', index_col=None, header=None)
 
 # The function:
-def combine(scaff_old, scaff_new, direct):
+def combine(scaffold, direct):
     """Combine all samples with GenomicsDBImport"""
     combine_cmd = "gatk --java-options \"-XX:ParallelGCThreads=1 -Xmx100g -Djava.io.tmpdir={}\" GenomicsDBImport ".format(scratch_dir)
     for i in vcf_dir:
         combine_cmd += "--variant {}{} ".format(direct,i)
     combine_cmd += "--tmp-dir {} ".format(scratch_dir)
-    combine_cmd += "--genomicsdb-workspace-path {}genomicDBI_{} ".format(scratch_dir, scaff_new)
-    combine_cmd += "-L {} ".format(scaff_old)
+    combine_cmd += "--genomicsdb-workspace-path {}genomicDBI_{} ".format(scratch_dir, scaffold)
+    combine_cmd += "-L {} ".format(scaffold)
     """Create a .sh files with the combine variant functions."""
-    file = open('{}combine_genomicDBImport_{}.sh'.format(direct, scaff_new),'w')
+    file = open('{}combine_genomicDBImport_{}.sh'.format(direct, scaffold),'w')
     file.write('#!/bin/bash \n')
     file.write('#SBATCH --account={} \n'.format(account))
     file.write('#SBATCH --mem 110G \n')
-    file.write('#SBATCH --cpus-per-task=1 \n')
+    file.write('#SBATCH -cpus-per-task=1 \n')
     file.write('#SBATCH --time=10:00:00 \n')
 ##    file.write('#SBATCH --time=250:00:00 \n')
     file.write(combine_cmd)
     file.write('\n')
-    file.write('cp -a {}genomicDBI_{} {}genomicDBI_{}'.format(scratch_dir, scaff_new, direct, scaff_new))
+    file.write('cp -a {}genomicDBI_{} {}genomicDBI_{}'.format(scratch_dir, scaffold, direct, scaffold))
     file.close()
     ##"""Submit the .sh to the server"""
-    sub_cmd = "sbatch -o {}combine_genomicDBImport_{}.out {}combine_genomicDBImport_{}.sh".format(direct, scaff_new, direct, scaff_new)
+    sub_cmd = "sbatch -o {}combine_genomicDBImport_{}.out {}combine_genomicDBImport_{}.sh".format(direct, scaffold, direct, scaffold)
     subprocess.call(sub_cmd, shell=True)
 
 
@@ -79,12 +79,11 @@ if all(list_exist):
     subprocess.call(mv_call, shell=True)
     print("\t Move the call log files")
     for scaff in range(0, nb_scaff):
-        scaff_old=genome_assembly[0][scaff]
-        scaff_new=genome_assembly[0][scaff + 1]
+        scaffold=genome_assembly[0][scaff]
         vcf_dir=[]
         for name in bamfile_dir:
-            vcf_dir.append("{}_{}_res.g.vcf".format(name, scaff_new))
-        print("Combine for scaffold/chromosome {} called {}".format(scaff_new, scaff_old))
-        combine(scaff_old=scaff_old, scaff_new=scaff_new, direct=direct)
+            vcf_dir.append("{}_{}_res.g.vcf".format(name, scaffold))
+        print("Combine for scaffold {} called {}".format(scaffold, scaffold))
+        combine(scaffold=scaffold, direct=direct)
 else:
     print("\t Some res.g.vcf file are missing --> PROBLEM")
