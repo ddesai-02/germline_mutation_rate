74870854475acf18e6c5263fffe06cb74bd85a90
= <=>
2024-06-14 10:39:09 -0400

    capitalization error

--- variable.py
+++ variable.py
@@ -1,6 +1,6 @@
 # Enter species name and reference genome name without the ".fa" extension:
 sp="Odocoileus_virginianus"                     # Species name
-refGenome="Odocoileus_virginianus_NEW"          # Name of reference genome
+refGenome="odocoileus_virginianus_NEW"          # Name of reference genome
 path="/home/devan/projects/rrg-shaferab/devan"  # Your project directory
 nb_scaff=482                                    # Number of scaffolds
 account="rrg-shaferab"                          # Your SBATCH account
