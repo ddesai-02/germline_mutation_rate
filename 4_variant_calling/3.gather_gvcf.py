7832e951aded5832c8535af9163302bc875692ae
= <=>
2024-06-17 11:51:26 -0400

    Generalized and changed to scaffolds.

--- 4_variant_calling/3.gather_gvcf.py
+++ 4_variant_calling/3.gather_gvcf.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-This script gather:
-    the genotypes per chromosomes into one files
-    the back combine per chromosomes into one file
+This script gathers the genotypes per chromosomes into one file
+    then combines them back per scaffold/chromosomes into one file
 """
-##################################################
-# What you need ##################################
-##################################################
+#################
+# What you need #
+#################
 
 # Packages:
 import subprocess
@@ -16,23 +15,23 @@ import pandas as pd
 
 # Directories:
 direct = "{}/{}/vcf_files/".format(path, sp)
-chrom_dir = "{}/{}/".format(path, sp)
+scaff_dir = "{}/{}/".format(path, sp)
 
-# Import chrom names:
-chrom_name = pd.read_csv('{}chromosomes.txt'.format(chrom_dir),sep=' ', index_col=None, header=None)
+# Import chrom/scaff names:
+genome_assembly = pd.read_csv('{}scaffolds.txt'.format(scaff_dir),sep=' ', index_col=None, header=None)
 
 # List of the files:
 back_dir = []
 geno_dir = []
-for line in range(0,nb_chrom):
-    chrom=chrom_name.loc[line,1]
-    back_dir.append("back_combine_genomicDBI_{}.g.vcf".format(chrom))
-    geno_dir.append("genotype_genomicDBI_{}.g.vcf".format(chrom))
+for line in range(0,nb_scaff):
+    scaff=genome_assembly.loc[line,1]
+    back_dir.append("back_combine_genomicDBI_{}.g.vcf".format(scaff))
+    geno_dir.append("genotype_genomicDBI_{}.g.vcf".format(scaff))
 
 
 # The function:
 def gather(direct, list_file, output, what):
-    """Gather each chrom together"""
+    """Gather each chrom/scaff together"""
     gather_cmd = "gatk --java-options \"-XX:ParallelGCThreads=16 -Xmx120g \" GatherVcfs "
     for i in list_file:
         gather_cmd += "-I {}{} ".format(direct, i)
@@ -40,8 +39,9 @@ def gather(direct, list_file, output, what):
     """Create a .sh files with the gather functions."""
     file = open('{}gather_genomicDBImport_{}.sh'.format(direct, what),'w')
     file.write('#!/bin/bash \n')
+    file.write('#SBATCH --account={} \n'.format(account))
     file.write('#SBATCH --mem 124G \n')
-    file.write('#SBATCH -c 16 \n')
+    file.write('#SBATCH --cpus-per-task=16 \n')
     file.write('#SBATCH --time=12:00:00 \n')
     file.write(gather_cmd)
     file.write('\n')
@@ -51,15 +51,15 @@ def gather(direct, list_file, output, what):
     subprocess.call(sub_cmd, shell=True)
 
 
-##################################################
-# What you run  ##################################
-##################################################
+################
+# What you run #
+################
 
-# Gather if everything exist for all chromosomes:
+# Gather if everything exist for all scaffolds/chromosomes:
 list_exist=[]
-for line in range(0, nb_chrom):
-    chrom=chrom_name.loc[line,1]
-    list_exist.append(os.path.exists("{}back_combine_genomicDBI_{}.g.vcf".format(direct, chrom)))
+for line in range(0, nb_scaff):
+    scaff=genome_assembly.loc[line,1]
+    list_exist.append(os.path.exists("{}back_combine_genomicDBI_{}.g.vcf".format(direct, scaff)))
 if all(list_exist):
     print("\t All back_combine directories exist --> back combine done")
     mv_bc= "mv {}back_combine_genomicDBImport_* {}back_com.log/".format(direct, direct)
@@ -69,9 +69,9 @@ if all(list_exist):
     print("Gather back combine")
 
 list_exist=[]
-for line in range(0, nb_chrom):
-    chrom=chrom_name.loc[line,1]
-    list_exist.append(os.path.exists("{}genotype_genomicDBI_{}.g.vcf".format(direct, chrom)))
+for line in range(0, nb_scaff):
+    scaff=genome_assembly.loc[line,1]
+    list_exist.append(os.path.exists("{}genotype_genomicDBI_{}.g.vcf".format(direct, scaff)))
 if all(list_exist):
     print("\t All genotypes directories exist --> genotype done")
     mv_geno= "mv {}genotype_genomicDBImport_* {}back_com.log/".format(direct, direct)
