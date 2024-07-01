bb1900b5e11e2e17bda47fdb38e58b8aa64e529c
Devan Desai <devandesai@trentu.ca>
2024-07-01 11:02:13 -0400

    Fixed 6_DNMs/variable.py variable.py

--- 6_DNMs/variable.py
+++ 6_DNMs/variable.py
@@ -5,5 +5,11 @@ path="/home/devan/projects/rrg-shaferab/devan"              # Your project direc
 nb_scaff=482                                                # Number of scaffolds
 account="rrg-shaferab"                                      # Your SBATCH account
 scratch_dir="/home/devan/scratch/"                          # Your scratch directory
-ntfy_server="http://ntfy.metri.cc/germline_mutation_rate"   #ntfy server, set to empty if you're not using it
+ntfy_server="http://ntfy.metri.cc/germline_mutation_rate" #ntfy server, set to empty if you're not using it
 
+## ONLY FOR 6_DMNs
+GQ_lim="60"
+DP_min="0.5*mean_dp"
+DP_max="2*mean_dp"
+AB_max="0.7"
+AB_min="0.3"
