61b9797563caf7e567b79fd8e561f8d473084dd2
Devan Desai <devandesai@trentu.ca>
2024-07-24 13:56:38 -0400

    1_trimming/1_filtering/submit_split.sh Small syntax fix

--- 1_trimming/1_filtering/submit_split.sh
+++ 1_trimming/1_filtering/submit_split.sh
@@ -8,7 +8,7 @@ while read p; do
         echo "#!/bin/bash" > submit_split/submit_clean_$COUNT
         echo "#SBATCH --account=rrg-shaferab" >> submit_split/submit_clean_$COUNT
         echo "#SBATCH --mem-per-cpu 20G" >> submit_split/submit_clean_$COUNT
-        echo "#SBATCH -cpus-per-task=10" >> submit_split/submit_clean_$COUNT
+        echo "#SBATCH --cpus-per-task=10" >> submit_split/submit_clean_$COUNT
         echo "#SBATCH --time=12:00:00" >> submit_split/submit_clean_$COUNT
         echo $p >> submit_split/submit_clean_$COUNT
         ##
