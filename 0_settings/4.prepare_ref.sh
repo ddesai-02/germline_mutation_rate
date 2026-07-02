eab3def1bc8742d56b1c9fefd55701f54e021da0
Devan Desai <93961382+ddesai-02@users.noreply.github.com>
2026-07-02 13:07:56 -0400

    Update 4.prepare_ref.sh

--- 0_settings/4.prepare_ref.sh
+++ 0_settings/4.prepare_ref.sh
@@ -2,6 +2,8 @@
 # The reference genome should be indexed with:
 source ../variable.py
 
+module load bwa java samtools
+
 echo $path/$sp/ref_fasta/$refGenome
 bwa index -p $path/$sp/ref_fasta/$refGenome -a bwtsw $path/$sp/ref_fasta/$refGenome.fa
 java -jar $EBROOTPICARD/picard.jar CreateSequenceDictionary R=$path/$sp/ref_fasta/$refGenome.fa O=$path/$sp/ref_fasta/$refGenome.dict
